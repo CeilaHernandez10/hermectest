f944c845239f0c925fddb40fce36c82aaab7ff33
CeilaHernandez10 <49285943+CeilaHernandez10@users.noreply.github.com>
2019-04-12 14:47:35 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
-'name': 'Interfaz de crear nuevo producto',
-'description': 'Campo precio de costo',
+'name': 'Modificando campo de precio de costo en  nuevo producto',
+'description': 'Editar las propiedades del campo, para hacerlo requerido',
 'author': 'Ceila Hernandez',
 'depends' : ['sale']
 }
